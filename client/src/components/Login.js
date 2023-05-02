dbeca473cdd8c6e94a76f3f9a0b5adb587e7f575
abhishekrd <abhishekrdhanke636@gmail.com>
2023-05-02 22:22:30 +0530

    feat : add loaders between req&res delay🔃

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -7,13 +7,16 @@ const Login = () => {
   const navigate = useNavigate();
   const [email,setEmail] = useState("");
   const [password,setPassword] = useState("");
+  const [loading, setLoading] = useState(false);
 
   const loginHandler = async (e) => {
      e.preventDefault();
      if(!email || !password){
        return alert("Please enter all the fields!")
      }
-     const userData = await fetch("http://localhost:5000/signin",{
+    try{
+      setLoading(true)
+      const userData = await fetch("http://localhost:5000/signin",{
         method:"POST",
         headers:{
           "Content-type":"application/json"
@@ -23,7 +26,7 @@ const Login = () => {
           password
         })
      })
-     
+
      const jsonUser = await userData.json();
      console.log(jsonUser);
      alert("Sign in Successful!");
@@ -31,10 +34,20 @@ const Login = () => {
      localStorage.setItem("jwt", jsonUser.token);
      localStorage.setItem("user", JSON.stringify(jsonUser.user))
      navigate("/create");
+
+    }
+    catch(err){
+      console.log(err);
+    }
+    finally{
+       setLoading(false)
+    }  
+    
   }
 
   return (
-    <div className='bg-blue-50 flex justify-center items-center h-screen'>
+    <> 
+    {loading ? <div className='h-screen bg-blue-100 flex justify-center items-center'><span className="loader"></span></div> :  <div className='bg-blue-50 flex justify-center items-center h-screen'>
         <div className='bg-white rounded-lg shadow-xl flex justify-center items-center flex-col px-10 py-14 gap-4'>
               <p className='text-3xl font'><span className="text-blue-600">Note</span>Wise</p>
               <input type='email' className='font text-lg px-8 py-2 border-2 rounded-lg outline-none transition-all transition-duration-500 shadow-sm focus:shadow-xl focus:border-2 focus:border-blue-600' placeholder='Enter your Email' onChange={(e) => setEmail(e.target.value)}></input>
@@ -42,7 +55,9 @@ const Login = () => {
               <button className='font bg-blue-600 hover:bg-blue-500 active:scale-95 transition-all transition-duration-500 outline-none px-6 py-1 rounded-lg border-none hover:shadow-xl text-white hover:text-black' id='LoginBtn' onClick={loginHandler}>LOGIN</button>
               <p className='text-sm font'>Don't have account? <Link to="/signup" className='text-blue-500'>Sign up</Link> here</p>
         </div>
-    </div>
+    </div>}
+   
+    </>
   )
 }
 
--- client/src/components/Signup.js
+++ client/src/components/Signup.js
@@ -7,13 +7,17 @@ const Signup = () => {
   const [name, setName] = useState("");
   const [email,setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [loading,setLoading] = useState(false);
 
   const registerUser = async (e) => {
         e.preventDefault();
         if(!name || !email || !password){
             return alert("Please enter all the fields!")
         }
+       
+        
         try {
+            setLoading(true)
             const user = await fetch("http://localhost:5000/signup",{
                 method:"POST",
                 headers:{
@@ -32,20 +36,27 @@ const Signup = () => {
         } catch (error) {
             console.log(error);
         }
+        finally{
+            setLoading(false)
+        }
 
   }
 
   return (
+    <>
+    {loading ? <div className='h-screen bg-blue-100 flex justify-center items-center'><span className="loader"></span></div> :
     <div className='bg-blue-50 flex justify-center items-center h-screen'>
-        <div className='bg-white rounded-lg shadow-xl flex justify-center items-center flex-col px-10 py-14 gap-4'>
-              <p className='text-3xl font'><span className="text-blue-600">Note</span>Wise</p>
-              <input type='text' className='font text-lg px-8 py-2 border-2 rounded-lg outline-none transition-all transition-duration-500 shadow-sm focus:shadow-xl focus:border-2 focus:border-blue-600' placeholder='Enter your Name' onChange={(e) => setName(e.target.value)} ></input>
-              <input type='email' className='font text-lg px-8 py-2 border-2 rounded-lg outline-none transition-all transition-duration-500 shadow-sm focus:shadow-xl focus:border-2 focus:border-blue-600' placeholder='Enter your Email' onChange={(e) => setEmail(e.target.value)} ></input>
-              <input type='password' className='font text-lg px-8 py-2 border-2 rounded-lg outline-none transition-all transition-duration-500 shadow-sm focus:shadow-xl focus:border-2 focus:border-blue-600' placeholder='Enter your Password'  onChange={(e) => setPassword(e.target.value)}></input>
-              <button className='font bg-blue-600 hover:bg-blue-500 active:scale-95 transition-all transition-duration-500 outline-none px-6 py-2 rounded-lg border-none hover:shadow-xl text-white hover:text-black' id='LoginBtn' onClick={registerUser} >SIGN UP</button>
-              <p className='text-sm font'>Already have account? <Link to="/login" className='text-blue-500'>Login</Link> here</p>
-        </div>
+    <div className='bg-white rounded-lg shadow-xl flex justify-center items-center flex-col px-10 py-14 gap-4'>
+          <p className='text-3xl font'><span className="text-blue-600">Note</span>Wise</p>
+          <input type='text' className='font text-lg px-8 py-2 border-2 rounded-lg outline-none transition-all transition-duration-500 shadow-sm focus:shadow-xl focus:border-2 focus:border-blue-600' placeholder='Enter your Name' onChange={(e) => setName(e.target.value)} ></input>
+          <input type='email' className='font text-lg px-8 py-2 border-2 rounded-lg outline-none transition-all transition-duration-500 shadow-sm focus:shadow-xl focus:border-2 focus:border-blue-600' placeholder='Enter your Email' onChange={(e) => setEmail(e.target.value)} ></input>
+          <input type='password' className='font text-lg px-8 py-2 border-2 rounded-lg outline-none transition-all transition-duration-500 shadow-sm focus:shadow-xl focus:border-2 focus:border-blue-600' placeholder='Enter your Password'  onChange={(e) => setPassword(e.target.value)}></input>
+          <button className='font bg-blue-600 hover:bg-blue-500 active:scale-95 transition-all transition-duration-500 outline-none px-6 py-2 rounded-lg border-none hover:shadow-xl text-white hover:text-black' id='LoginBtn' onClick={registerUser} >SIGN UP</button>
+          <p className='text-sm font'>Already have account? <Link to="/login" className='text-blue-500'>Login</Link> here</p>
     </div>
+</div>}
+    
+    </>
   )
 }
 
